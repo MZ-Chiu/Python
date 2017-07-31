13afce09b1c85ee34b1b714dc7779f8b8d0029fc
Simon <qmz1592@gmail.com>
2017-07-31 23:34:46 +0800

    Use pycharm for platise

---
+++ platise/test/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def max(a, b):
+    if a >= b:
+        return a
+    else:
+        return b
+
+print("请输入Somthing:")
--- platise/test/tt.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-
-print("Hello , python!")
-print("Hello , python!")
-print("1024 * 123 = ", 1024 *123);
