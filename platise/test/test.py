c4383e0ef0498e88de34b45254b010e753e0d60a
Simon <qmz1592@gmail.com>
2017-08-16 00:02:12 +0800

    Class, objcet..

--- platise/test/test.py
+++ platise/test/test.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-def max(a, b):
-    if a >= b:
-        return a
-    else:
-        return b
+print("# 类和实例")
 
-print("请输入Somthing:")
+
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
+        print('%s: %s' % (self.name, self.score))
+
+
+print("# 访问限制")
+
+
+class Human(object):
+    def __init__(self, name, len):
+        self.__name = name
+        self.__len = len
+
+    def print_len(self):
+        print('%s: %s' % (self.__name, self.__len))
+
+
+
+print("# 继承和多态")
+
+
